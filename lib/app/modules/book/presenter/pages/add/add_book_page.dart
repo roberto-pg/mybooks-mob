84e9253f09adc768bfc3302d3bab0648be33bc48
Roberto <rpg61@hotmail.com>
2021-12-15 10:55:10 -0300

    Removes unnecessary dependencies

--- lib/app/modules/book/presenter/pages/add/add_book_page.dart
+++ lib/app/modules/book/presenter/pages/add/add_book_page.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:http_parser/http_parser.dart';
 import 'package:image_picker/image_picker.dart';
--- lib/app/modules/book/presenter/pages/detail/detail_page.dart
+++ lib/app/modules/book/presenter/pages/detail/detail_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_triple/flutter_triple.dart';
 import 'package:google_fonts/google_fonts.dart';
