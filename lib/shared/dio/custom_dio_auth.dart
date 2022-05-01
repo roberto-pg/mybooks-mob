2dd986593664239fce705fae9613c8ede7005c2e
Roberto <roberto.p.gomes@hotmail.com>
2022-04-30 23:08:44 -0300

    Remove .env

--- lib/shared/dio/custom_dio_auth.dart
+++ lib/shared/dio/custom_dio_auth.dart
@@ -2,16 +2,13 @@ import 'package:dio/dio.dart';
 import 'package:dio/native_imp.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-import '../config.dart';
 import 'interceptors/auth_interceptor.dart';
 
 class CustomDioAuth extends DioForNative {
   final FlutterSecureStorage _storage;
-  
-
 
   static final _baseOptions = BaseOptions(
-    baseUrl: Config.baseUrl,
+    baseUrl: const String.fromEnvironment('baseUrl'),
     connectTimeout: 20000,
     receiveTimeout: 20000,
   );
